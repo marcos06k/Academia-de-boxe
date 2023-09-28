6dbf6ad9387e55d333619c71aa5ad025fb758d2f
marcos06k <biedermannmarcos@gmail.com>
2023-09-28 13:28:40 -0300

    Quase tudo pronto, so falta mostra o valor final na tabela

--- assets/script/codigoLoja.js
+++ assets/script/codigoLoja.js
@@ -1,8 +1,23 @@
 
-let infoRecebida = localStorage.getItem("info")
+let arrayRecebidoNomeString = localStorage.getItem('arrayNome');
+let arrayRecebidoNome= JSON.parse(arrayRecebidoNomeString);
 
+console.log(arrayRecebidoNome);
 
+let arrayRecebidoValorString = localStorage.getItem('arrayValor');
+let arrayRecebidoValor= JSON.parse(arrayRecebidoValorString);
 
-    console.log(infoRecebida)
+console.log(arrayRecebidoValor);
+
+    let tabela = document.getElementById("tabela_produto")
+    let pedidoMin = 10000000
+    let pedidoMax = 99000000
+
+    for (let i = 0; i < arrayRecebidoNome.length; i++) {
+        tabela.innerHTML += "<tr> <td>"+ arrayRecebidoNome[i] + "</td> <td> R$ "+ parseFloat(arrayRecebidoValor[i]).toFixed(2) + "</td> <td> N° "+ Math.floor(Math.random() * (pedidoMax - pedidoMin + 1) + pedidoMin) + "</td> </tr>"
+        console.log("t");
+        
+    }
+
   
 
