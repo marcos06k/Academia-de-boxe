73fd2dc29ab395f24775bb6c76c5d38ac4c6b927
Your Name <you@example.com>
2023-09-28 18:49:12 -0300

    completo e funcional a loja e carrinho

--- assets/script/codigoLoja.js
+++ assets/script/codigoLoja.js
@@ -12,12 +12,16 @@ console.log(arrayRecebidoValor);
     let tabela = document.getElementById("tabela_produto")
     let pedidoMin = 10000000
     let pedidoMax = 99000000
+    let valorTotalPedido = 0
 
     for (let i = 0; i < arrayRecebidoNome.length; i++) {
         tabela.innerHTML += "<tr> <td>"+ arrayRecebidoNome[i] + "</td> <td> R$ "+ parseFloat(arrayRecebidoValor[i]).toFixed(2) + "</td> <td> N° "+ Math.floor(Math.random() * (pedidoMax - pedidoMin + 1) + pedidoMin) + "</td> </tr>"
         console.log("t");
         
+        valorTotalPedido += parseFloat(arrayRecebidoValor[i])
     }
 
+
+     tabela.innerHTML += "<tr> <td> Total: </td> <td> R$ "+ valorTotalPedido.toFixed(2) + "</td> <td></td> </tr>"
   
 
